603a666ae1337ea22b4821698ed53d1e9ab5a21a
Dustin Peukert <dpeukert04@gmail.com>
2024-11-04 13:08:43 -0500

    write BikeClub class

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,3 +1,25 @@
 class BikeClub
+  attr_reader :name, :bikers
 
+  def initialize(name)
+    @name = name
+    @bikers = []
+  end
+
+  def add_biker(biker)
+    @bikers << biker
+  end
+
+  def most_rides
+    most = @bikers[0]
+
+    @bikers.each do |biker|
+      current_most_rides = most.rides.values.flatten.count
+      num_of_rides = biker.rides.values.flatten.count
+
+      most = biker if num_of_rides > current_most_rides
+    end
+
+    most
+  end
 end

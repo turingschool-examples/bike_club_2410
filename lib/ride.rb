adb30ae7e1de77072f7750e39504c135cd70840f
elysableu <elysabward@gmail.com>
2024-11-04 09:50:24 -0700

    Add initialize method and attr_reader

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,10 @@
 class Ride
+  attr_reader :name, :distance, :terrain
 
+  def initialize(ride_data)
+    @name = ride_data[:name]
+    @distance = ride_data[:distance]
+    @loop = ride_data[:loop]
+    @terrain = ride_data[:terrain]
+  end
 end

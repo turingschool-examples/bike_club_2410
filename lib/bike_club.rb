bb3107568b9215a01c0913f8decadc8a3cfbb3e3
elysableu <elysabward@gmail.com>
2024-11-04 11:16:32 -0700

    Add add_biker method

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,8 +1,15 @@
 class BikeClub
-  attr_reader :name, bikers
+  attr_reader :name, :bikers
 
   def initialize(name)
     @name = name
     @bikers = []
   end
+
+  def add_biker(biker)
+    @bikers << biker
+  end
+
+ 
+
 end

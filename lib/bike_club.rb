59b8a29674da6c4fe4c6c4bc989aececcac9c5c6
Jillian Bleggi <jillian.bleggi@gmail.com>
2024-11-04 13:14:55 -0500

    Iteration 3 completed

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -17,4 +17,10 @@ class BikeClub
   def best_time(ride)
     @bikers.min_by { |biker| biker.personal_record(ride) }
   end
+
+  def bikers_eligible(ride)
+    @bikers.select do |biker|
+      biker.acceptable_terrains.include?(ride.terrain) && biker.max_distance >= ride.total_distance
+    end
+  end
 end
--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -74,13 +74,35 @@ RSpec.describe BikeClub do
       bikeclub.add_biker(biker1)
       bikeclub.add_biker(biker2) 
 
-
       expect(bikeclub.best_time(ride1)).to eq biker1
-
     end
 
-    xit 'can tell which Bikers are eligible for a given ride' do
+    it 'can tell which Bikers are eligible for a given ride' do
+      biker1 = Biker.new("Kenny", 30)
+      biker1.learn_terrain!(:gravel)
+      biker1.learn_terrain!(:hills)
+
+      biker2 = Biker.new("Athena", 30)
+      biker2.learn_terrain!(:gravel)
+      biker2.learn_terrain!(:hills)
+
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel}) 
+
+      biker1.log_ride(ride1, 92.5)
+      biker1.log_ride(ride1, 91.1)
+      biker1.log_ride(ride2, 60.9)
+      biker1.log_ride(ride2, 61.6)
       
+      biker2.log_ride(ride1, 95.0)
+      biker2.log_ride(ride2, 65.0)
+
+      bikeclub = BikeClub.new("Sons of Anarchy")
+      bikeclub.add_biker(biker1)
+      bikeclub.add_biker(biker2) 
+      
+      expect(bikeclub.bikers_eligible(ride1)).to eq [biker1, biker2]
+
     end
   end
 end

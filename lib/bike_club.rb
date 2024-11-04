89166bad1e0ef8cc42b4e7dade50b6ac9423d51e
Kevin Newland <kevin.newland@yahoo.com>
2024-11-04 12:53:54 -0600

    write methods for BikeClub

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,3 +1,21 @@
 class BikeClub
+  attr_reader :name, :bikers
+  def initialize(name)
+    @name = name
+    @bikers = []
+  end
 
-end
+  def add_biker(biker)
+    @bikers << biker
+  end
+
+  def most_rides #almost just trying to find out how i can shovel the biker with the most rides into array then output it.
+    biker_most_rides = [] #also this should proabably not be an array should be a single biker
+
+    @bikers.each do |biker|
+      biker.rides.count 
+
+      biker_most_rides << biker
+    end
+  end
+end 

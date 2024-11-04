fed67974b70b2114616ec42d57ecad9e2cf09943
Jono Sommers <jonosommers95@gmail.com>
2024-11-04 12:54:06 -0600

    time is up

--- lib/biker.rb
+++ lib/biker.rb
@@ -1,3 +1,35 @@
 class Biker
 
+    attr_reader :name, :max_distance, :rides, :acceptable_terrain
+
+    def initialize(name, max_distance)
+        @name = name
+        @max_distance = max_distance
+        @rides = {}
+        @acceptable_terrain = []
+    end
+
+    def learn_terrain!(terrain)
+        @acceptable_terrain << terrain 
+    end
+
+    def log_ride(ride, time)
+        if @acceptable_terrain.include?(ride.terrain) && @max_distance >= ride.distance
+            @rides[ride] ||= []
+            @rides[ride] << time
+        end
+    end
+
+    def personal_record(ride)
+        if @rides.include?(ride)
+            @rides.find do |r, time|
+                if r == ride
+                    @best_time = time.min
+                end
+            end
+            @best_time
+        else
+            false
+        end
+    end
 end

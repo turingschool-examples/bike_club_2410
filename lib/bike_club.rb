6aa171d9d563a929abbf6beb22d5834bec245198
elysableu <elysabward@gmail.com>
2024-11-04 11:59:48 -0700

    Add best_time method

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -11,10 +11,35 @@ class BikeClub
   end
 
   def most_rides
-    return @bikers.max_by do |biker|
-      biker.rides.count
+    biker_total_rides = {}
+    @bikers.each do |biker|
+      biker_total_rides[biker] = 0
+      biker.rides.each do |ride|
+        biker_total_rides[biker] += ride[1].count
+      end
     end
+
+    most_rides = biker_total_rides.max_by do |biker| 
+      biker[1]
+    end
+
+    most_rides[0]
   end
 
-  
+  def best_time(ride)
+    
+    eligible_bikers = @bikers.find_all do |biker|
+      biker_ride = biker.rides.keys
+      biker_ride.include?(ride)
+    end
+
+    best_timed = eligible_bikers.min_by do |biker|
+      biker.personal_record(ride)
+      
+    end
+    
+    best_timed
+  end
+
+ 
 end

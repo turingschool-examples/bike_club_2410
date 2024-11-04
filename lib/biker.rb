5deb66b48d573a048f046e4902becb8e2b6a10e8
Kristin Weiland <kristinweiland@gmail.com>
2024-11-04 10:20:18 -0800

    Complete the log ride and personal record methods and add their helper methods.

--- lib/biker.rb
+++ lib/biker.rb
@@ -12,17 +12,52 @@ class Biker
         @acceptable_terrain = acceptable_terrain
     end
 
+    def accepted_terrain?(terrain)
+        if acceptable_terrain.include?(terrain)
+            true
+        else
+            false
+        end
+    end
+
+    def accepted_distance?(distance)
+        if max_distance >= distance
+            true
+        else
+            false
+        end
+    end
+
     def learn_terrain!(terrain)
         acceptable_terrain << terrain
     end
 
     def log_ride(ride, time)
-        if rides[ride]
-            rides[ride] << time
+        if accepted_terrain?(ride.terrain) && accepted_distance?(ride.total_distance)
+            if rides[ride]
+                rides[ride] << time
+            else
+                rides[ride] = [time]
+            end
+            return "#{self.name} knows this terrain and can bike this distance"
+        elsif accepted_terrain?(ride.terrain) && !accepted_distance?(ride.total_distance)
+           return "#{self.name} can't bike this distance"
+        elsif accepted_distance?(ride.total_distance) && !accepted_terrain?(ride.terrain)  
+           return "#{self.name} doesn't know this terrain yet"
         else
-            rides[ride] = [time]
+            return "#{self.name} can't bike this distance and doesn't know this terrain yet"
         end
         rides
     end
+
+    def personal_record(ride)
+        if rides[ride] 
+            ordered_times = rides[ride].sort
+            ordered_times.first
+        else 
+            false
+        end
+    end
+
 
 end

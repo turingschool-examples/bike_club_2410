c4788179ab0f9c86decd42f920958f15400c1da3
elysableu <elysabward@gmail.com>
2024-11-04 09:34:47 -0700

    Add tests for log_ride

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -5,6 +5,14 @@ RSpec.describe Biker do
   # Iteration 2
   before(:each) do
     @biker = Biker.new("Kenny", 30)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", 
+                      distance: 10.7, 
+                      loop: false, 
+                      terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", 
+                      distance: 14.9, 
+                      loop: true, 
+                      terrain: :gravel})
   end
 
   describe "#initialize" do
@@ -40,6 +48,31 @@ RSpec.describe Biker do
       @biker.learn_terrain!(:hills)
 
       expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
+    end
+  end
+
+  describe "#log_ride" do
+    it 'can log a rides' do
+      expect(@biker.log_ride(@ride1, 92.5)).to be true
+      expect(@biker.log_ride(@ride2, 60.9)).to be true
+    end
+
+    it 'can log multiple times for different rides' do
+      expect(@biker.log_ride(@ride1, 92.5)).to be true
+      expect(@biker.log_ride(@ride1, 91.1)).to be true
+
+      expect(@biker.log_ride(@ride2, 60.9)).to be true
+      expect(@biker.log_ride(@ride2, 61.6)).to be true
+    end
+
+    it 'can log the rides in the rides log' do
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expect(@biker.rides).to eq({@ride1 => [92.5, 91.1],
+                                  @ride2 => [60.9, 61.6]})
     end
   end
 end

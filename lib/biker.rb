7bb0710a2a25e9479ed18a641a45c866b7ca2eb5
Kristin Weiland <kristinweiland@gmail.com>
2024-11-04 09:25:18 -0800

    Add the log ride method and tests.

--- lib/biker.rb
+++ lib/biker.rb
@@ -12,4 +12,17 @@ class Biker
         @acceptable_terrain = acceptable_terrain
     end
 
+    def learn_terrain!(terrain)
+        acceptable_terrain << terrain
+    end
+
+    def log_ride(ride, time)
+        if rides[ride]
+            rides[ride] << time
+        else
+            rides[ride] = [time]
+        end
+        rides
+    end
+
 end
--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -21,5 +21,37 @@ RSpec.describe Biker do
         end
     end
 
+    describe '#terrain' do
+        it 'can learn terrain' do
+            biker = Biker.new("Kenny", 30)
 
+            biker.learn_terrain!(:gravel)
+            expect(biker.acceptable_terrain).to eq([:gravel])
+
+            biker.learn_terrain!(:hills)
+            expect(biker.acceptable_terrain).to eq([:gravel, :hills])
+        end
+    end
+
+    describe '#rides' do
+        it 'can log rides' do
+            biker = Biker.new("Kenny", 30)
+
+            ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+
+            expect(ride1).to be_an_instance_of(Ride)
+
+            ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+            expect(ride2).to be_an_instance_of(Ride)
+
+            biker.log_ride(ride1, 92.5)
+            expect(biker.rides).to eq({ride1 => [92.5]})
+            biker.log_ride(ride1, 91.1)
+            expect(biker.rides).to eq({ride1 => [92.5, 91.1]})
+            biker.log_ride(ride2, 60.9)
+            expect(biker.rides).to eq({ride1 => [92.5, 91.1], ride2 => [60.9]})
+            biker.log_ride(ride2, 61.6)
+            expect(biker.rides).to eq({ride1 => [92.5, 91.1], ride2 => [60.9, 61.6]})
+        end
+end
 end
